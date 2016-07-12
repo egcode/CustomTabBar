c5065de1e86ffd47f4ffee45b70890ea7f547b22
Eugene Golovanov <eugene.golovanov@hart.com>
2016-07-12 11:38:25 -0700

    buttons no init inside custom tab bar

--- CustomTabBar/CustomTabBarController.swift
+++ CustomTabBar/CustomTabBarController.swift
@@ -20,11 +20,12 @@ class CustomTabBarController: UITabBarController {
     //MARK: - Properties
 
     //Public
-    var buttonsArray = Array<UIButton>() {
-        didSet {
-            self.setupCircleButtons()
-        }
-    }
+    var buttonsArray = Array<UIButton>()
+//        {
+//        didSet {
+//            self.setupCircleButtons()
+//        }
+//    }
     var circleButtonsSize:CGFloat = 50
     
     //Private
@@ -85,6 +86,9 @@ class CustomTabBarController: UITabBarController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        
+        //Circle Buttons
+        self.setupCircleButtons()
         self.view.bringSubviewToFront(menuButton)
     }
     
@@ -115,6 +119,94 @@ class CustomTabBarController: UITabBarController {
     
     
     private func setupCircleButtons() {
+        
+        //Circle buttons Size
+        self.circleButtonsSize = 50
+        
+        //Adding Buttons
+        let button1 = UIButton(frame: CGRectMake(
+            0,
+            0,
+            self.circleButtonsSize,
+            self.circleButtonsSize))
+        button1.setTitle("1", forState: .Normal)
+        button1.setTitleColor(UIColor.yellowColor(), forState: .Normal)
+        button1.addTarget(self, action: #selector(CustomTabBarController.button1Pressed), forControlEvents: .TouchUpInside)
+        button1.layer.cornerRadius = CGRectGetWidth(button1.frame)/2.0
+        button1.backgroundColor = UIColor.orangeColor()
+        button1.layer.masksToBounds = true
+        button1.alpha = 0.0
+        button1.enabled = false
+        
+        let button2 = UIButton(frame: CGRectMake(
+            0,
+            0,
+            self.circleButtonsSize,
+            self.circleButtonsSize))
+        button2.setTitle("2", forState: .Normal)
+        button2.setTitleColor(UIColor.yellowColor(), forState: .Normal)
+        button2.addTarget(self, action: #selector(CustomTabBarController.button2Pressed), forControlEvents: .TouchUpInside)
+        button2.enabled = false
+        button2.alpha = 0.0
+        button2.layer.cornerRadius = CGRectGetWidth(button2.frame)/2.0
+        button2.backgroundColor = UIColor.orangeColor()
+        button2.layer.masksToBounds = true
+        
+        
+        let button3 = UIButton(frame: CGRectMake(
+            0,
+            0,
+            self.circleButtonsSize,
+            self.circleButtonsSize))
+        button3.setTitle("3", forState: .Normal)
+        button3.setTitleColor(UIColor.yellowColor(), forState: .Normal)
+        button3.addTarget(self, action: #selector(CustomTabBarController.button3Pressed), forControlEvents: .TouchUpInside)
+        button3.enabled = false
+        button3.alpha = 0.0
+        button3.layer.cornerRadius = CGRectGetWidth(button3.frame)/2.0
+        button3.backgroundColor = UIColor.orangeColor()
+        button3.layer.masksToBounds = true
+        
+        let button4 = UIButton(frame: CGRectMake(
+            0,
+            0,
+            self.circleButtonsSize,
+            self.circleButtonsSize))
+        button4.setTitle("4", forState: .Normal)
+        button4.setTitleColor(UIColor.yellowColor(), forState: .Normal)
+        button4.addTarget(self, action: #selector(CustomTabBarController.button4Pressed), forControlEvents: .TouchUpInside)
+        button4.enabled = false
+        button4.alpha = 0.0
+        button4.layer.cornerRadius = CGRectGetWidth(button3.frame)/2.0
+        button4.backgroundColor = UIColor.orangeColor()
+        button4.layer.masksToBounds = true
+        
+        
+        let button5 = UIButton(frame: CGRectMake(
+            0,
+            0,
+            self.circleButtonsSize,
+            self.circleButtonsSize))
+        button5.setTitle("5", forState: .Normal)
+        button5.setTitleColor(UIColor.yellowColor(), forState: .Normal)
+        button5.addTarget(self, action: #selector(CustomTabBarController.button5Pressed), forControlEvents: .TouchUpInside)
+        button5.enabled = false
+        button5.alpha = 0.0
+        button5.layer.cornerRadius = CGRectGetWidth(button3.frame)/2.0
+        button5.backgroundColor = UIColor.orangeColor()
+        button5.layer.masksToBounds = true
+        
+        
+        //Adding buttons
+        self.buttonsArray.append(button5)
+        self.buttonsArray.append(button4)
+        self.buttonsArray.append(button3)
+        self.buttonsArray.append(button2)
+        self.buttonsArray.append(button1)
+
+        
+        
+        //Align All Buttons
         for button in self.buttonsArray {
             button.frame = CGRectMake(
                 self.view.frame.size.width/2 - button.frame.size.width/2,
@@ -124,6 +216,8 @@ class CustomTabBarController: UITabBarController {
             self.view.addSubview(button)
         }
     }
+    
+    
 
     private func setupEffectsView() {
         let someFrame = CGRect(
@@ -154,6 +248,9 @@ class CustomTabBarController: UITabBarController {
 
     }
     
+    
+    
+    
     //------------------------------------------------------------------------------------------------------
     //MARK: - Actions
     
@@ -175,6 +272,29 @@ class CustomTabBarController: UITabBarController {
         
 
     }
+    
+    
+    
+    
+    
+    func button1Pressed() {
+        print("PRESSED 1-1-1")
+    }
+    
+    func button2Pressed() {
+        print("PRESSED 2-2-2")
+    }
+    func button3Pressed() {
+        print("PRESSED 3-3-3")
+    }
+    
+    func button4Pressed() {
+        print("PRESSED 4-4-4")
+    }
+    func button5Pressed() {
+        print("PRESSED 5-5-5")
+    }
+
     
     
     //------------------------------------------------------------------------------------------------------
